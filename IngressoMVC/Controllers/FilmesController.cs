cdf9d77d237a962529f33e0acb874a8e500cf6d6
Aecio Brito Ideapad <aeciobrito@gmail.com>
2022-06-28 09:19:49 -0300

    Metodo de criacao de filme, com atores e categorias

--- IngressoMVC/Controllers/FilmesController.cs
+++ IngressoMVC/Controllers/FilmesController.cs
@@ -2,10 +2,7 @@
 using IngressoMVC.Models;
 using IngressoMVC.Models.ViewModels.RequestDTO;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace IngressoMVC.Controllers
 {
@@ -18,20 +15,11 @@ namespace IngressoMVC.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
-        {
-            return View(_context.Filmes);
-        }
+        public IActionResult Index() => View(_context.Filmes);
 
-        public IActionResult Detalhes(int id)
-        {
-            return View(_context.Filmes.Find(id));
-        }
+        public IActionResult Detalhes(int id) => View(_context.Filmes.Find(id));
 
-        public IActionResult Criar()
-        {
-            return View();
-        }
+        public IActionResult Criar() => View();
 
         [HttpPost]
         public IActionResult Criar(PostFilmeDTO filmeDto)
@@ -53,9 +41,58 @@ namespace IngressoMVC.Controllers
                 );
 
             _context.Add(filme);
+            _context.SaveChanges();            
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult CriarFilmeComCategoriasAtores(PostFilmeDTO filmeDto)
+        {
+            var cinema = _context.Cinemas.FirstOrDefault(c => c.Nome == filmeDto.NomeCinema);
+            if (cinema == null) return View();
+
+            var produtor = _context.Produtores.FirstOrDefault(p => p.Nome == filmeDto.NomePodutor);
+            if (produtor == null) return View();
+
+            Filme filme = new Filme
+                (
+                    filmeDto.Titulo,
+                    filmeDto.Descricao,
+                    filmeDto.Preco,
+                    filmeDto.ImageURL,
+                    cinema.Id,
+                    produtor.Id
+                );
+
+            _context.Add(filme);
             _context.SaveChanges();
 
             //Incluir Relacionamentos
+            foreach(var categoria in filmeDto.Categorias)
+            {
+                int? categoriaId = _context.Categorias.Where(c => c.Nome == categoria).FirstOrDefault().Id;
+
+                if (categoriaId != null)
+                {
+                    var novaCategoria = new FilmeCategoria(filme.Id, categoriaId.Value);
+                    _context.FilmesCategorias.Add(novaCategoria);
+                    _context.SaveChanges();
+                }
+            }
+
+            foreach(var ator in filmeDto.NomeAtores)
+            {
+                int? atorId = _context.Atores.Where(a => a.Nome == ator).FirstOrDefault().Id;
+
+                if(atorId != null)
+                {
+                    var novoAtor = new AtorFilme(atorId.Value, filme.Id);
+                    _context.AtoresFilmes.Add(novoAtor);
+                    _context.SaveChanges();
+                }
+            }
+
             return RedirectToAction(nameof(Index));
         }
 
